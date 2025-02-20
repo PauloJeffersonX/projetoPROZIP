ff966eb0208ad4f602123e7f8a15765d9d55fdc1
PauloJeffersonX <paulojefferson89@gmail.com>
2025-02-19 22:17:31 -0300

    fisrt

---
+++ index.py
@@ -0,0 +1,14 @@
+def calculadora(num1, num2, operacao):
+ 
+  if operacao == 1:
+    return num1 + num2
+  elif operacao == 2:
+    return num1 - num2
+  elif operacao == 3:
+    return num1 * num2
+  elif operacao == 4:
+    if num2 == 0:  # Evita divisão por zero
+      return 0 
+    return num1 / num2
+  else:
+    return 0
